2172c20c0ecc61bd88cf37a659b40fd5f5d5a234
Jannis R <mail@jannisr.de>
2020-05-16 15:49:12 +0200

    basic test :white_check_mark:

--- example.js
+++ example.js
@@ -1,3 +1,18 @@
 'use strict'
 
-// todo
+const {JSDOM} = require('jsdom')
+const {readFileSync} = require('fs')
+const {
+	findComments,
+} = require('.')
+
+const post = readFileSync(require.resolve('./test/hn-post.html'), {encoding: 'utf8'})
+
+const {document} = new JSDOM(post).window
+
+const comments = findComments(document)
+while (true) {
+	const {value, done} = comments.next()
+	if (done) break
+	console.log(value)
+}
--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
 'use strict'
 
-const todo = require('..')
+const {JSDOM} = require('jsdom')
+const {readFileSync} = require('fs')
+const {
+	deepStrictEqual: dEql,
+} = require('assert')
+const {
+	findComments,
+} = require('..')
 
-// todo
+// fixtures
+
+const post = readFileSync(require.resolve('./hn-post.html'), {encoding: 'utf8'})
+const {document} = new JSDOM(post).window
+
+const expectedAll = require('./hn-post.json')
+
+// tests
+
+const all = findComments(document)
+dEql(Array.from(all), expectedAll, 'all deep-equal')
+console.info('all ✔︎')
